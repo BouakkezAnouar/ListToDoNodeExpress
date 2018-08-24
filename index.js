45dcfd53893aa764bdf969a2bb2c1a38f2ead66f
BouakkezAnouar <anouar.bouakkez@gmail.com>
2018-08-24 16:41:48 +0100

    add a session to the app

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const todo = require("./routes/todo");
+const session = require("cookie-session");
 const app = express(); // app
 
+app.use(session({ secret: "todotopsecret" }));
+app.use((req, res, next) => {
+  if (typeof req.session.todos == "undefined") req.session.todos = [];
+  next();
+});
 app.use(express.json()); //middelware
 app.use(express.urlencoded({ extended: true }));
 app.use("/todo", todo); //route todo
